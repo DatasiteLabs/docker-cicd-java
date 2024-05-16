107a1813d08da1a54f9175ecfa62124aea9868c9
Kayla Altepeter <kayla.altepeter@datasite.com>
2024-05-16 11:45:51 -0500

    feat: gradle 7.x (#9)

--- test.sh
+++ test.sh
@@ -5,8 +5,11 @@ set -o nounset
 [[ ${DEBUG:-} == true ]] && set -o xtrace
 readonly __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-LOCAL_NAME=datasite/test-cicd-java
+DOCKER_REPO=${1:-0.0.0.0:5001}
+LOCAL_NAME=${DOCKER_REPO}/datasite/test-cicd-java
 
 echo "Running tests against ${LOCAL_NAME}"
-docker run --rm -v "${__dir}/test:/home/gradle/test" -w /home/gradle/test --user gradle "${LOCAL_NAME}:latest" bash -c "ls -la;"
-docker run -it --user gradle test-cicd-java-consumer:latest
+
+docker run -it --user gradle "${LOCAL_NAME}-consumer:latest-java"
+
+LOCAL_NAME=datasite/test-cicd-java
